ee98ae8f288df9ba80057eefd09515ddea7ec729
Marc Hermans <marc.hermans@ldtteam.com>
2022-09-03 12:14:17 +0200

    Add base delegating smart model which can be used when dynamically baking smart models which pull data from a delegate.

---
+++ core/src/main/java/com/communi/suggestu/scena/core/client/models/baked/base/BaseDelegatingSmartModel.java
@@ -0,0 +1,51 @@
+package com.communi.suggestu.scena.core.client.models.baked.base;
+
+import com.communi.suggestu.scena.core.client.models.IDelegatingBakedModel;
+import net.minecraft.client.renderer.block.model.ItemTransforms;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.model.BakedModel;
+import org.jetbrains.annotations.NotNull;
+
+public abstract class BaseDelegatingSmartModel extends BaseSmartModel implements IDelegatingBakedModel
+{
+
+    private final BakedModel delegate;
+
+    protected BaseDelegatingSmartModel(final BakedModel delegate) {this.delegate = delegate;}
+
+    @Override
+    public BakedModel getDelegate()
+    {
+        return delegate;
+    }
+
+    @Override
+    public boolean useAmbientOcclusion()
+    {
+        return getDelegate().useAmbientOcclusion();
+    }
+
+    @Override
+    public boolean isGui3d()
+    {
+        return getDelegate().isGui3d();
+    }
+
+    @Override
+    public boolean isCustomRenderer()
+    {
+        return getDelegate().isCustomRenderer();
+    }
+
+    @Override
+    public @NotNull TextureAtlasSprite getParticleIcon()
+    {
+        return getDelegate().getParticleIcon();
+    }
+
+    @Override
+    public @NotNull ItemTransforms getTransforms()
+    {
+        return getDelegate().getTransforms();
+    }
+}
