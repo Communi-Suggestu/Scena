953e285cc041a011093dc41a09f863ada0d291b0
Marc Hermans <marc.hermans@ldtteam.com>
2024-02-10 19:07:21 +0100

    Fix model data load on forge.

--- neoforge/src/main/java/com/communi/suggestu/scena/forge/mixin/platform/client/ActiveModelDataManagerMixin.java
+++ neoforge/src/main/java/com/communi/suggestu/scena/forge/mixin/platform/client/ActiveModelDataManagerMixin.java
@@ -1,5 +1,8 @@
 package com.communi.suggestu.scena.forge.mixin.platform.client;
 
+import com.communi.suggestu.scena.core.client.models.data.IBlockModelData;
+import com.communi.suggestu.scena.core.entity.block.IBlockEntityWithModelData;
+import com.communi.suggestu.scena.forge.platform.client.model.data.ForgeBlockModelDataPlatformDelegate;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -17,6 +20,19 @@ public abstract class ActiveModelDataManagerMixin
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/entity/BlockEntity;getModelData()Lnet/neoforged/neoforge/client/model/data/ModelData;")
     )
     private ModelData bypassExpensiveCalculationIfNecessary(BlockEntity instance, Operation<ModelData> original) {
-            return original.call(instance);
+        if (instance instanceof IBlockEntityWithModelData blockEntityWithModelData) {
+            final IBlockModelData blockModelData = blockEntityWithModelData.getBlockModelData();
+            if (blockModelData == null) {
+                return ModelData.EMPTY;
+            }
+
+            if (!(blockModelData instanceof ForgeBlockModelDataPlatformDelegate platformDelegate)) {
+                throw new IllegalStateException("Block model data is not compatible with the current platform.");
+            }
+
+            return platformDelegate.getDelegate();
+        }
+
+        return original.call(instance);
     }
 }
