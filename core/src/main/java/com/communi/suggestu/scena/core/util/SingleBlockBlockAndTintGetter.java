bd1363a95999896c38c65e342fe08543fda38fb0
Marc Hermans <marc.hermans@ldtteam.com>
2026-04-21 07:40:54 +0200

    Add support for biome color blending in BlockAndTintGetter

--- core/src/main/java/com/communi/suggestu/scena/core/util/SingleBlockBlockAndTintGetter.java
+++ core/src/main/java/com/communi/suggestu/scena/core/util/SingleBlockBlockAndTintGetter.java
@@ -1,13 +1,18 @@
 package com.communi.suggestu.scena.core.util;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.BlockAndTintGetter;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Cursor3D;
+import net.minecraft.util.ARGB;
 import net.minecraft.world.level.BlockAndLightGetter;
 import net.minecraft.world.level.CardinalLighting;
 import net.minecraft.world.level.ColorResolver;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.lighting.LevelLightEngine;
+import net.minecraft.world.level.material.FluidState;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -78,6 +83,86 @@ public class SingleBlockBlockAndTintGetter extends SingleBlockBlockReader implem
         public Builder withSource(final @Nullable BlockAndTintGetter source)
         {
             this.source = source;
+            return this;
+        }
+
+        public Builder withSource(final @Nullable Level level)
+        {
+            if (level == null) {
+                this.source = null;
+                return this;
+            }
+
+            this.source = new BlockAndTintGetter() {
+                @Override
+                public CardinalLighting cardinalLighting()
+                {
+                    return level.dimensionType().cardinalLightType().get();
+                }
+
+                @Override
+                public int getBlockTint(final BlockPos pos, final ColorResolver colorResolver)
+                {
+                    int dist = Minecraft.getInstance().options.biomeBlendRadius().get();
+                    if (dist == 0) {
+                        return colorResolver.getColor(level.getBiome(pos).value(), pos.getX(), pos.getZ());
+                    } else {
+                        int count = (dist * 2 + 1) * (dist * 2 + 1);
+                        int totalRed = 0;
+                        int totalGreen = 0;
+                        int totalBlue = 0;
+                        Cursor3D cursor = new Cursor3D(pos.getX() - dist, pos.getY(), pos.getZ() - dist, pos.getX() + dist, pos.getY(), pos.getZ() + dist);
+                        BlockPos.MutableBlockPos nextPos = new BlockPos.MutableBlockPos();
+
+                        while (cursor.advance()) {
+                            nextPos.set(cursor.nextX(), cursor.nextY(), cursor.nextZ());
+                            int color = colorResolver.getColor(level.getBiome(nextPos).value(), nextPos.getX(), nextPos.getZ());
+                            totalRed += ARGB.red(color);
+                            totalGreen += ARGB.green(color);
+                            totalBlue += ARGB.blue(color);
+                        }
+
+                        return ARGB.color(totalRed / count, totalGreen / count, totalBlue / count);
+                    }
+                }
+
+                @Override
+                public LevelLightEngine getLightEngine()
+                {
+                    return level.getLightEngine();
+                }
+
+                @Override
+                public @org.jspecify.annotations.Nullable BlockEntity getBlockEntity(final BlockPos pos)
+                {
+                    return level.getBlockEntity(pos);
+                }
+
+                @Override
+                public BlockState getBlockState(final BlockPos pos)
+                {
+                    return level.getBlockState(pos);
+                }
+
+                @Override
+                public FluidState getFluidState(final BlockPos pos)
+                {
+                    return level.getFluidState(pos);
+                }
+
+                @Override
+                public int getHeight()
+                {
+                    return level.getHeight();
+                }
+
+                @Override
+                public int getMinY()
+                {
+                    return level.getMinY();
+                }
+            };
+
             return this;
         }
 
